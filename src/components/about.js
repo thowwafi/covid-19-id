01ffe8dc9afc64c24adf2e32bb850e8c8c58cc3e
As'ad Saleh Umar <asad@rubyh.co>
2020-04-02 23:05:22 +0700

    Add GoogleDataStudio

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 class About extends React.Component {
   render() {
-    return <h1>About</h1>
+    return (
+      <div>
+        <FullScreenWrapper
+          // width="600"
+          // height="450"
+          src="https://datastudio.google.com/embed/reporting/0da1c48e-0971-4e9d-9d44-e37963d6c278/page/cytKB"
+          frameborder="0"
+          allowfullscreen
+        ></FullScreenWrapper>
+      </div>
+    );
   }
 }
-export default About
+export default About;
+
+const FullScreenWrapper = styled.iframe`
+  width: 100%;
+  height: 100vh;
+`;
--- src/components/provinces.js
+++ src/components/provinces.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const Provinces = ({provinces}) => {
-    return (
-        <div className="header-text">
-            <center><h1>Data per Provinsi</h1></center>            
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        {provinces.map((province) => (
-                            <div key={province.id} className="card mb-3">
-                                <div className="card-body">
-                                    <h6 className="card-title">{province.provinsi}</h6>
-                                    <span className="card-subtitle mb-2 mr-3">Positif: {province.kasusPosi}</span>
-                                    <span className="card-subtitle mb-2 mr-3">Sembuh: {province.kasusSemb}</span>
-                                    <span className="card-subtitle mb-2 mr-3">Meninggal: {province.kasusMeni}</span>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+const Provinces = ({ provinces }) => {
+  return (
+    <div className="header-text">
+      <center>
+        <h1>Data per Provinsi</h1>
+      </center>
+      <div className="container">
+        <div className="row">
+          {provinces.map(province => (
+            <div className="col-xs-12 col-md-3">
+              <div key={province.id} className="card mb-3">
+                <div className="card-body">
+                  <h6 className="card-title">{province.provinsi}</h6>
+                  <span className="card-subtitle mb-2 mr-3">
+                    Positif: {province.kasusPosi}
+                  </span>
+                  <span className="card-subtitle mb-2 mr-3">
+                    Sembuh: {province.kasusSemb}
+                  </span>
+                  <span className="card-subtitle mb-2 mr-3">
+                    Meninggal: {province.kasusMeni}
+                  </span>
                 </div>
+              </div>
             </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
-export default Provinces
+export default Provinces;
