966839f0cae1db554b90a47fe8881ff20d84e727
As'ad Saleh Umar <asad@rubyh.co>
2020-04-02 22:56:46 +0700

    BIkin chart versi 1

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className="header-text">
-            <div className="footer">
-                <nav className="navbar fixed-bottom navbar-light bg-dark">
-                    <a className="navbar-brand" href="/"><Link to='/'>Home</Link></a>
-                    <a className="navbar-brand" href="/"><Link to='/chart'>Chart</Link></a>
-                    <a className="navbar-brand" href="/"><Link to='/about'>Tentang</Link></a>
-                </nav>
-            </div>
-        </div>
-    )
+  return (
+    <div className="header-text">
+      <div className="footer">
+        <nav className="navbar fixed-bottom navbar-light bg-dark">
+          <Link to="/" className="navbar-brand">
+            Home
+          </Link>
+          <Link to="/chart" className="navbar-brand">
+            Chart
+          </Link>
+          <Link to="/about" className="navbar-brand">
+            Tentang
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
-export default Footer
+export default Footer;
